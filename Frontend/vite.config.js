818ae0c5316157d16916588e1a9821f48773c62e
Sankar <sankarjyotichetia57@gmail.com>
2024-12-02 18:12:33 +0530

    proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://video-tube-api-lime.vercel.app/",
+      "/api": "https://video-tube-api-lime.vercel.app/api/v1",
     },
   },
   plugins: [react()],
