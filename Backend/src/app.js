154681d7bbeb45875432bbae5701af343b149d2a
Sankar <sankarjyotichetia57@gmail.com>
2024-12-04 11:59:19 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: [
+            "https://video-tube-lake.vercel.app",
+            "http://localhost:5173",
+        ],
+        credentials: true,
+    })
+);
+
 
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
