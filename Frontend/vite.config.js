88b43b4819a57a3b68db976adb21738d82d9963e
sankar111 <sankarjyotichetia0@gmail.com>
2025-01-12 08:21:43 +0530

    fix: update axios base URL and Vite proxy configuration for production environment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "",
+      "/api": "https://video-tube-seven.vercel.app/",
     },
   },
   plugins: [react()],
