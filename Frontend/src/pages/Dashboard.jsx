b6ae39d814505f7466ca5dc766f677dc5782640b
sankar <sankarjyotichetia57@gmail.com>
2025-02-06 09:07:43 +0530

    fixing dashbooard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -8,40 +8,38 @@ import { getChannelStats } from "../hooks/getChannelStats.js";
 import { getChannelVideos } from "../hooks/getChannelVideos.js";
 
 function Dashboard() {
-    const dispatch = useDispatch();
-    const { status, userData } = useSelector((state) => state.auth);
-    const [statsloading, setStatsLoading] = useState(true);
-    const [videoloading, setVideoLoading] = useState(true);
+  const dispatch = useDispatch();
+  const { status, userData } = useSelector((state) => state.auth);
+  const [statsloading, setStatsLoading] = useState(true);
+  const [videoloading, setVideoLoading] = useState(true);
 
-    useEffect(() => {
-        if (status) {
-            getChannelVideos(dispatch).then(() => setVideoLoading(false));
-            getChannelStats(dispatch, userData._id).then(() =>
-                setStatsLoading(false)
-            );
-        }
-    }, []);
-
-    const { videos, stats } = useSelector((state) => state.dashboard);
-
-    if (!status) {
-        return <GuestDashboard />;
+  useEffect(() => {
+    if (status) {
+      getChannelVideos(dispatch).then(() => setVideoLoading(false));
+      getChannelStats(dispatch, userData._id).then(() =>
+        setStatsLoading(false)
+      );
     }
+  }, []);
 
-    if (videoloading || statsloading) {
-        return (
-            <span className="flex justify-center mt-20">
-                {icons.bigLoading}
-            </span>
-        );
-    } else {
-        return (
-            <div className="mx-auto flex w-full max-w-7xl flex-col gap-y-6 px-4 py-8">
-                <ChannelStats stats={stats} />
-                <VideoPanel channelVideos={videos} />
-            </div>
-        );
-    }
+  const { videos, stats } = useSelector((state) => state.dashboard);
+
+  if (!status) {
+    return <GuestDashboard />;
+  }
+
+  if (videoloading || statsloading) {
+    return (
+      <span className="flex justify-center mt-20">{icons.bigLoading}</span>
+    );
+  } else {
+    return (
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-y-6 px-4 py-8">
+        <ChannelStats stats={stats} />
+        <VideoPanel channelVideos={videos} />
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
