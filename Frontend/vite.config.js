eec527090b8c32211643165a4b399d80f180b466
sankar <sankarjyotichetia57@gmail.com>
2025-02-12 08:24:00 +0530

    m

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://video-tube-seven.vercel.app/",
+      "/api": "https://videotube-rmvp.onrender.com",
     },
   },
   plugins: [react()],
