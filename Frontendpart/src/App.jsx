fb963715d520a77447ff91fcadb1e2f476e23193
sankar <sankarjyotichetia57@gmail.com>
2025-02-12 09:56:01 +0530

    new frontend

--- Frontendpart/src/App.jsx
+++ Frontendpart/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { healthCheck } from "./hooks/healthCheck";
+import { getCurrentUser } from "./hooks/getCurrentUser";
+import { icons } from "./assets/Icons.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    healthCheck().then(() => {
+      getCurrentUser(dispatch).then(() => {
+        setLoading(false);
+      });
+    });
+    setInterval(() => {
+      healthCheck();
+    }, 5 * 60 * 1000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full  overflow-y-auto bg-[#121212] text-white">
+        <div className="flex flex-col items-center justify-center mt-64">
+          <span>{icons.loading}</span>
+          <h1 className="text-3xl text-center mt-8 font-semibold">
+            Please wait...
+          </h1>
+          <h1 className="text-xl text-center mt-4">
+            Refresh the page if it takes too long
+          </h1>
+        </div>
+      </div>
+    );
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-screen bg-black text-white flex flex-col bg-opacity-95">
+      <Navbar />
+      <div className="w-full h-full flex overflow-auto">
+        <div>
+          <Sidebar />
+        </div>
+        <main
+          className="overflow-y-auto h-full w-full scrollbar-hide"
+          id="scrollableDiv"
+        >
+          <Outlet />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div id="popup-models" className="bg-pink-400 relative"></div>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontendpart/src/hooks/getUserSubscribed.js
@@ -0,0 +1,16 @@
+import axiosInstance from "../utils/axios.helper";
+import { addUserSubscribed } from "../store/userSlice";
+
+export const getUserSubscribed = async (dispatch, subscriberId) => {
+  try {
+    const response = await axiosInstance.get(
+      `/subscriptions/u/${subscriberId}`
+    );
+    if (response?.data?.success) {
+      dispatch(addUserSubscribed(response.data.data));
+      return response.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
